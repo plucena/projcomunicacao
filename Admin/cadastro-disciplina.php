1a3a32e8a9c22885179b985e78ba8a5020ad2d63
diogoatc <diogol_l@hotmail.com>
2017-05-16 13:41:03 -0300

    Ajuste quantidade de créditos da disciplina

--- Admin/cadastro-disciplina.php
+++ Admin/cadastro-disciplina.php
@@ -107,6 +107,7 @@ include('../model/conexao.php');
       <option value="3">3</option>
       <option value="4">4</option>
       <option value="5">5</option>
+      <option value="6">6</option>
     </select>
   </div>
 </div>
